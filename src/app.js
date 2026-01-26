75ceaba5f6d8cf5b67e1fc584c0b0d4ecf31f423
kanass20 <kanass20@gmail.com>
2026-01-27 00:47:34 +0100

    Initial import of DevOps Task Manager project

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let tasks = [
+  { id: 1, title: "Initial task", completed: false }
+];
+
+app.get('/', (req, res) => {
+  res.json({ message: "Task Manager API running..." });
+});
+
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
+});
+
+app.post('/tasks', (req, res) => {
+  const newTask = { id: tasks.length+1, title: req.body.title, completed: false };
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+});
+
+app.listen(3000, ()=> console.log("API running on port 3000"));
